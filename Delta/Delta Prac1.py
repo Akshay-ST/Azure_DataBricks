c51475f106fc0446ffa77c3f06719dd0977ae8fb
th.akshay.singh@outlook.com <th.akshay.singh@outlook.com>
2024-06-06 07:11:14 +0000

    file updated

--- Delta/Delta Prac1.py
+++ Delta/Delta Prac1.py
@@ -141,3 +141,132 @@ df.write \
 
 # MAGIC %sql
 # MAGIC select * from retail_db.oders_delta2 where order_Status = 'COMPLETE' limit 10;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Inserting into DELTA TABLE<br>
+# MAGIC 1. Insert command<br>
+# MAGIC 2. Append<br>
+# MAGIC 3. Copy command<br>
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from retail_db.orders_delta 
+# MAGIC where order_status = 'CANCELED' 
+# MAGIC   and date(order_date) < '2013-07-28';
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe history retail_db.orders_delta
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC insert into retail_db.orders_delta values ('11111111','2013-07-25 00:00:00.0','22222222','CLOSED')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe history retail_db.orders_delta
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from retail_db.orders_delta 
+# MAGIC where order_status = 'CLOSED' 
+# MAGIC   and date(order_date) < '2013-07-26';
+
+# COMMAND ----------
+
+dfnew = spark.read.csv('/mnt/files/ordersappend.csv', header = True)
+
+dfnew.write.mode("append") \
+  .partitionBy("order_status") \
+  .format("delta") \
+  .save("/mnt/files/delta/orders.delta")
+
+
+
+
+# COMMAND ----------
+
+dfnew.show()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe history retail_db.orders_delta
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC copy into retail_db.orders_delta 
+# MAGIC from '/mnt/files/orders1.csv' 
+# MAGIC fileformat = csv 
+# MAGIC format_options('header'='true')
+# MAGIC --COPY_OPTIONS ('mergeSchema'='true')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe history retail_db.orders_delta
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC copy into retail_db.orders_delta 
+# MAGIC from '/mnt/files/orders_4R_5C.csv' 
+# MAGIC fileformat = csv 
+# MAGIC format_options('header'='true')
+# MAGIC --COPY_OPTIONS ('mergeSchema'='true')
+
+# COMMAND ----------
+
+df_new = spark.read.csv('/mnt/files/orders_4R_5C.csv', header = True)
+df_new.show()
+
+
+# COMMAND ----------
+
+df_new.write.mode("append") \
+  .partitionBy("order_status") \
+  .format("delta") \
+  .option("mergeSchema","true") \
+  .save("/mnt/files/delta/orders.delta")
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC select * from retail_db.orders_delta
+# MAGIC where order_amount is not null
+# MAGIC    --or date(order_date) = '2013-07-25';
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe history retail_db.orders_delta
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC update retail_db.orders_delta 
+# MAGIC set order_status = 'CLOSED' 
+# MAGIC where order_id = 2222222
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe history retail_db.orders_delta
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls dbfs:/databricks-datasets
+
+# COMMAND ----------
+
+
