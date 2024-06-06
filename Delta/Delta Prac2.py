bafd09dffc5e96bd06210996bf2a2e969a24dbef
th.akshay.singh@outlook.com <th.akshay.singh@outlook.com>
2024-06-06 15:20:25 +0000

    file update

--- Delta/Delta Prac2.py
+++ Delta/Delta Prac2.py
@@ -71,3 +71,52 @@ trip_df.repartition(20) \
 
 # MAGIC %sql
 # MAGIC describe extended trip_db.trips_parquet
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select min(Fare_Amt), max(Fare_Amt)
+# MAGIC from trip_db.trips_delta;
+# MAGIC
+# MAGIC -- 1.03 sec
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select min(Fare_Amt), max(Fare_Amt)
+# MAGIC from trip_db.trips_parquet;
+# MAGIC
+# MAGIC -- 10.62 sec
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*)
+# MAGIC from trip_db.trips_delta;
+# MAGIC
+# MAGIC -- 0.82 sec
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*)
+# MAGIC from trip_db.trips_parquet;
+# MAGIC
+# MAGIC -- 1.15 sec
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC select * from trip_db.trips_delta
+# MAGIC where Total_Amt = 234
+# MAGIC
+# MAGIC -- 1.36
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC select * from trip_db.trips_parquet
+# MAGIC where Total_Amt = 234
+# MAGIC
+# MAGIC -- 1.67
+# MAGIC
